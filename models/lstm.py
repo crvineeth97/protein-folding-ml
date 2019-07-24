Need to fix RMSD calculation

models/lstm.py
@@ -34,7 +34,7 @@ class LSTMModel(nn.Module):
         for i, prot in enumerate(primary):
             if embedding == "one_hot":
                 onehot = torch.zeros(
-                    [MAX_PROTEIN_LENGTH, 20], dtype=torch.float64, device=self.device
+                    [MAX_PROTEIN_LENGTH, 20], dtype=torch.float32, device=self.device
                 )
                 onehot = onehot.scatter_(1, prot.view(-1, 1).type(torch.long), 1)
                 features.append(onehot)

models/resnet.py
@@ -45,16 +45,19 @@ class ResNet(nn.Module):
         # output                        [n, 41, L]
         return torch.cat((transformed_primary, transformed_evolutionary), dim=1)
 
-    def generate_target(self, lengths, dihedrals):
+    def generate_target(self, lengths, phi, psi, omega):
         # dihedrals are in degrees
-        target = torch.zeros(
-            MINIBATCH_SIZE, len(dihedrals) * 2, lengths[0], device=DEVICE
-        )
+        target = torch.zeros(MINIBATCH_SIZE, 6, lengths[0], device=DEVICE)
         for i in range(MINIBATCH_SIZE):
-            for j, angles in enumerate(dihedrals):
-                angles = torch.from_numpy(angles * pi / 180.0).to(DEVICE)
-                target[i, j * 2, : lengths[i]] = torch.sin(angles)
-                target[i, j * 2 + 1, : lengths[i]] = torch.cos(angles)
+            ph = torch.from_numpy(phi[i] * pi / 180.0)
+            ps = torch.from_numpy(psi[i] * pi / 180.0)
+            om = torch.from_numpy(omega[i] * pi / 180.0)
+            target[i, 0, : lengths[i]] = torch.sin(ph)
+            target[i, 1, : lengths[i]] = torch.cos(ph)
+            target[i, 2, : lengths[i]] = torch.sin(ps)
+            target[i, 3, : lengths[i]] = torch.sin(ps)
+            target[i, 4, : lengths[i]] = torch.sin(om)
+            target[i, 5, : lengths[i]] = torch.sin(om)
         return target
 
     def calculate_loss(self, lengths, criterion, output, target):

preprocessing.py
@@ -58,7 +58,7 @@ def calculate_phi_from_masked_tertiary(tertiary_masked):
         points[0] = tertiary_masked[i - 1][6:]
         points[1:] = np.reshape(aa, (3, 3))
         phi.append(calculate_dihedral_from_points(points))
-    return np.array(phi, dtype=np.float) * 180.0 / np.pi
+    return np.array(phi, dtype=np.float32) * 180.0 / np.pi
 
 
 def calculate_psi_from_masked_tertiary(tertiary_masked):
@@ -85,7 +85,7 @@ def calculate_psi_from_masked_tertiary(tertiary_masked):
         points[0:3] = np.reshape(aa, (3, 3))
         points[3] = tertiary_masked[i + 1][:3]
         psi.append(calculate_dihedral_from_points(points))
-    return np.array(psi, dtype=np.float) * 180.0 / np.pi
+    return np.array(psi, dtype=np.float32) * 180.0 / np.pi
 
 
 def calculate_omega_from_masked_tertiary(tertiary_masked):
@@ -112,7 +112,7 @@ def calculate_omega_from_masked_tertiary(tertiary_masked):
         points[0:2] = np.reshape(aa[3:], (2, 3))
         points[2:] = np.reshape(tertiary_masked[i + 1][:6], (2, 3))
         omega.append(calculate_dihedral_from_points(points))
-    return np.array(omega, dtype=np.float) * 180.0 / np.pi
+    return np.array(omega, dtype=np.float32) * 180.0 / np.pi
 
 
 def masked_select(data, mask, X=None):
@@ -169,7 +169,7 @@ def read_protein(file_pointer):
             evolutionary = []
             for residue in range(21):
                 evolutionary.append(
-                    [float(step) for step in file_pointer.readline().split()]
+                    [np.float32(step) for step in file_pointer.readline().split()]
                 )
                 if len(evolutionary[-1]) != seq_len:
                     print(
@@ -200,7 +200,7 @@ def read_protein(file_pointer):
             tertiary = []
             for axis in range(3):
                 tertiary.append(
-                    [float(coord) for coord in file_pointer.readline().split()]
+                    [np.float32(coord) for coord in file_pointer.readline().split()]
                 )
                 if len(tertiary[-1]) != 3 * seq_len:
                     print(
@@ -267,7 +267,7 @@ def process_file(input_file, output_folder, save_tertiary):
         )
         evolutionary_reshaped = np.array(protein["evolutionary"]).T
         evolutionary_masked = np.array(
-            masked_select(evolutionary_reshaped, protein["mask"]), dtype=np.float
+            masked_select(evolutionary_reshaped, protein["mask"]), dtype=np.float32
         )
         # secondary_masked = np.array(
         #     masked_select(protein["secondary"], protein["mask"]), dtype=np.uint8
@@ -279,11 +279,13 @@ def process_file(input_file, output_folder, save_tertiary):
         # Putting np.zeros(9) as a signal of missing residues
         tertiary_masked = np.array(
             masked_select(tertiary_reshaped, protein["mask"], np.zeros(9)),
-            dtype=np.float,
+            dtype=np.float32,
         )
         # If the first few residues were missing, no need to consider them
         if np.allclose(tertiary_masked[0], np.zeros(9)):
             tertiary_masked = tertiary_masked[1:]
+        # TODO Check whether the last few residues need to be removed from tertiary
+
         # There are problems with some of the proteins in the dataset
         # Skip if that is the case
         skip_flg = 0
@@ -310,6 +312,7 @@ def process_file(input_file, output_folder, save_tertiary):
         assert len(omega) == masked_length
 
         if save_tertiary:
+            assert len(tertiary_masked) == masked_length
             np.savez(
                 output_folder + protein["id"] + ".npz",
                 primary=primary_masked,

training.py
@@ -31,16 +31,22 @@ def validate_model(model):
     rmsd = 0
     drmsd = 0
     for i, data in enumerate(validation_loader):
-        lengths, primary, evolutionary, phi, psi, omega, tertiary = data
+        # Tertiary is [Batch, Length, 9]
+        lengths, primary, evolutionary, act_phi, act_psi, act_omega, tertiary = data
         inp = model.generate_input(lengths, primary, evolutionary)
-        output = model(inp)
+        # Doesn't require gradients to go backwards, hence detach the output
+        output = model(inp).detach()
         # The following will be of size [Batch, Length]
-        phi = torch.atan2(output[:, 0, :], output[:, 1, :]).unsqueeze(1)
-        psi = torch.atan2(output[:, 2, :], output[:, 3, :]).unsqueeze(1)
-        omega = torch.atan2(output[:, 4, :], output[:, 5, :]).unsqueeze(1)
+        pred_phi = torch.atan2(output[:, 0, :], output[:, 1, :]).unsqueeze(1)
+        pred_psi = torch.atan2(output[:, 2, :], output[:, 3, :]).unsqueeze(1)
+        pred_omega = torch.atan2(output[:, 4, :], output[:, 5, :]).unsqueeze(1)
         # dihedrals will be of size [Length, Batch, 3] as this is the input
         # required by pnerf functions
-        dihedrals = torch.cat((phi, psi, omega), 1).transpose(1, 2).transpose(0, 1)
+        dihedrals = (
+            torch.cat((pred_phi, pred_psi, pred_omega), 1)
+            .transpose(1, 2)
+            .transpose(0, 1)
+        )
         # Pnerf takes dihedrals and optional bond lengths and bond angles as input
         # and builds the coordinates so that rmsd can be calculated for loss
         # Divide by 100 to get in angstrom units
@@ -53,15 +59,8 @@ def validate_model(model):
             )
             / 100
         )
-        coords = coordinates.transpose(0, 1).contiguous().view(1, -1, 9).transpose(0, 1)
-        predicted_coords = coords.transpose(0, 1).contiguous().view(-1, 3)
-        actual_coords = (
-            torch.from_numpy(tertiary)
-            .unsqueeze(1)
-            .transpose(0, 1)
-            .contiguous()
-            .view(-1, 3)
-        )
+        predicted_coords = coordinates.transpose(0, 1).contiguous().view(-1, 3)
+        actual_coords = tertiary
         rmsd += calc_rmsd(predicted_coords, actual_coords)
         drmsd += calc_drmsd(predicted_coords, actual_coords)
     rmsd /= val_size
@@ -90,21 +89,17 @@ def train_model(model):
     while not stopping_condition_met:
         optimizer.zero_grad()
         model.zero_grad()
-        for minibatch_id, training_minibatch in enumerate(train_loader):
-            # training_minibatch is a tuple of tuple of tensors except
+        for i, data in enumerate(train_loader):
+            # data is a tuple of tuple of tensors except
             # lengths, which is a tuple of ints
             # Implies primary will be a tuple with MINIBATCH_SIZE number of elements
             # And each element will be of shape (Length,)
             # phi, psi and omega are in degrees here
-            lengths, primary, evolutionary, actual_phi, actual_psi, actual_omega = (
-                training_minibatch
-            )
+            lengths, primary, evolutionary, act_phi, act_psi, act_omega = data
             # inp should be of shape [Batch, 41, Max_length]
             inp = model.generate_input(lengths, primary, evolutionary)
             # target should be of shape [Batch, 6, Max_length]
-            target = model.generate_target(
-                lengths, np.array([actual_phi, actual_psi, actual_omega])
-            )
+            target = model.generate_target(lengths, act_phi, act_psi, act_omega)
             # output should be of shape [Batch, 6, Max_length]
             # sin(phi), cos(phi), sin(psi), cos(psi), sin(omega), cos(omega)
             output = model(inp)
@@ -128,13 +123,13 @@ def train_model(model):
                 if not HIDE_UI:
                     data = {}
                     data["pdb_data_pred"] = open("output/pred_test.pdb", "r").read()
-                    data["pdb_data_true"] = open("output/actual_test.pdb", "r").read()
+                    data["pdb_data_true"] = open("output/act_test.pdb", "r").read()
                     data["validation_dataset_size"] = val_size
                     # data["sample_num"] = sample_num
                     data["train_loss_values"] = train_loss_values
                     data["validation_loss_values"] = validation_loss_values
-                    data["phi_actual"] = list(actual_phi)
-                    data["psi_actual"] = list(actual_psi)
+                    data["phi_actual"] = list(act_phi)
+                    data["psi_actual"] = list(act_psi)
                     pred_phi = np.arctan2(output[:, 0, :], output[:, 1, :])
                     pred_psi = np.arctan2(output[:, 2, :], output[:, 3, :])
                     # pred_omega = np.arctan2(output[:, 4, :], output[:, 5, :])