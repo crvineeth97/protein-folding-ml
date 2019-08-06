Preprocess degrees to radians Add best model validation, rmsd and best rmsd Remove empty directories during rsync Fix logging issues in preprocessing Create a new file and class for visualization Include testing in main

debug/dihedral_calculation.py
@@ -49,7 +49,7 @@ def calculate_phi_from_masked_tertiary(tertiary_masked):
         points[0] = tertiary_masked[i - 1][6:]
         points[1:] = np.reshape(aa, (3, 3))
         phi.append(calculate_dihedral_from_points(points))
-    return np.array(phi, dtype=np.float32) * 180.0 / np.pi
+    return np.array(phi, dtype=np.float32)
 
 
 def calculate_psi_from_masked_tertiary(tertiary_masked):
@@ -76,7 +76,7 @@ def calculate_psi_from_masked_tertiary(tertiary_masked):
         points[0:3] = np.reshape(aa, (3, 3))
         points[3] = tertiary_masked[i + 1][:3]
         psi.append(calculate_dihedral_from_points(points))
-    return np.array(psi, dtype=np.float32) * 180.0 / np.pi
+    return np.array(psi, dtype=np.float32)
 
 
 def calculate_omega_from_masked_tertiary(tertiary_masked):
@@ -103,7 +103,7 @@ def calculate_omega_from_masked_tertiary(tertiary_masked):
         points[0:2] = np.reshape(aa[3:], (2, 3))
         points[2:] = np.reshape(tertiary_masked[i + 1][:6], (2, 3))
         omega.append(calculate_dihedral_from_points(points))
-    return np.array(omega, dtype=np.float32) * 180.0 / np.pi
+    return np.array(omega, dtype=np.float32)
 
 
 def get_backbone_coords(pdb_id):

main.py
@@ -1,13 +1,17 @@
 import logging
 from time import time
-
-from constants import DEVICE
+import torch
+from constants import DEVICE, LEARNING_RATE
 from models.resnet import ResNet
 from preprocessing import preprocess_raw_data
 from training import train_model
+from testing import test_model
 from util import init_output_dir
 
 model = ResNet().to(DEVICE)
+criterion = torch.nn.MSELoss()
+# TODO Try various options of Adam
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 init_output_dir(model)
 
 start = time()
@@ -15,5 +19,9 @@ preprocess_raw_data()
 logging.info("Total preprocessing time: %f", time() - start)
 
 start = time()
-train_model_path = train_model(model)
+train_model(model, criterion, optimizer)
 logging.info("Total training time: %f", time() - start)
+
+start = time()
+test_model(model, criterion)
+logging.info("Total testing time: %f", time() - start)

models/resnet.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import numpy as np
 
 from constants import DEVICE, MINIBATCH_SIZE
 from models.resnet_1d import resnet98
@@ -49,14 +48,14 @@ class ResNet(nn.Module):
         return torch.cat((transformed_primary, transformed_evolutionary), dim=1)
 
     def generate_target(self, lengths, phi, psi, omega):
-        # dihedrals are in degrees
+        # dihedrals are in radians
         target = torch.zeros(
             MINIBATCH_SIZE, 4, lengths[0], device=DEVICE, dtype=torch.float32
         )
         for i in range(MINIBATCH_SIZE):
-            ph = torch.from_numpy(phi[i] * np.pi / 180.0)
-            ps = torch.from_numpy(psi[i] * np.pi / 180.0)
-            # om = torch.from_numpy(omega[i] * np.pi / 180.0)
+            ph = torch.from_numpy(phi[i])
+            ps = torch.from_numpy(psi[i])
+            # om = torch.from_numpy(omega[i])
             target[i, 0, : lengths[i]] = torch.sin(ph)
             target[i, 1, : lengths[i]] = torch.cos(ph)
             target[i, 2, : lengths[i]] = torch.sin(ps)

preprocessing.py
@@ -59,7 +59,7 @@ def calculate_phi_from_masked_tertiary(tertiary_masked):
         points[0] = tertiary_masked[i - 1][6:]
         points[1:] = np.reshape(aa, (3, 3))
         phi.append(calculate_dihedral_from_points(points))
-    return np.array(phi, dtype=np.float32) * 180.0 / np.pi
+    return np.array(phi, dtype=np.float32)
 
 
 def calculate_psi_from_masked_tertiary(tertiary_masked):
@@ -86,7 +86,7 @@ def calculate_psi_from_masked_tertiary(tertiary_masked):
         points[:3] = np.reshape(aa, (3, 3))
         points[3] = tertiary_masked[i + 1][:3]
         psi.append(calculate_dihedral_from_points(points))
-    return np.array(psi, dtype=np.float32) * 180.0 / np.pi
+    return np.array(psi, dtype=np.float32)
 
 
 def calculate_omega_from_masked_tertiary(tertiary_masked):
@@ -113,7 +113,7 @@ def calculate_omega_from_masked_tertiary(tertiary_masked):
         points[0:2] = np.reshape(aa[3:], (2, 3))
         points[2:] = np.reshape(tertiary_masked[i + 1][:6], (2, 3))
         omega.append(calculate_dihedral_from_points(points))
-    return np.array(omega, dtype=np.float32) * 180.0 / np.pi
+    return np.array(omega, dtype=np.float32)
 
 
 def masked_select(data, mask, X=None):
@@ -252,7 +252,7 @@ def read_protein(file_pointer):
 
 
 def process_file(input_file, output_folder, save_tertiary):
-    logging.info("Processing raw data file", input_file)
+    logging.info("Processing raw data file %s", input_file)
     input_file_pointer = open("data/raw/" + input_file, "r")
     idx = 0
 
@@ -355,7 +355,7 @@ def process_file(input_file, output_folder, save_tertiary):
                 omega=omega,
             )
         idx += 1
-        if idx % 2000 == 0:
+        if idx % 1000 == 0:
             logging.info("Last: %s - %d proteins processed", protein["id"], idx)
 
     input_file_pointer.close()

testing.py
@@ -1,28 +1,30 @@
-import torch
-import time
-import numpy as np
+import logging
 from os import listdir
+
+import numpy as np
+import torch
+
+from constants import MINIBATCH_SIZE, TESTING_FOLDER
 from dataloader import contruct_dataloader_from_disk
-from constants import TESTING_FOLDER, MINIBATCH_SIZE
-from training import init_plot
 from preprocessing import filter_input_files
+from visualize import Visualizer
 
 
 def compute_mae(lengths, pred, act):
+    # Compute MAE in degrees
     mae = 0
     for i in range(MINIBATCH_SIZE):
         tmp = 0
         for j in range(lengths[i]):
-            tmp += abs(pred[i][j] - act[i][j])
+            tmp += abs(pred[i][j] - act[i][j]) * 180.0 / np.pi
         tmp /= lengths[i]
         mae += tmp
     mae /= MINIBATCH_SIZE
     return mae
 
 
-def test_model(path, criterion):
-    model = torch.load(path, map_location={"cuda:0": "cpu"})
-    is_plt_initialized = False
+def test_model(model, criterion, sleep_time=0):
+    visualize = Visualizer()
     loss = 0
     phi_mae = 0
     psi_mae = 0
@@ -39,37 +41,27 @@ def test_model(path, criterion):
             loss += model.calculate_loss(lengths, criterion, output, target).item()
             # The following will be of size [Batch, Length]
             output = output.cpu().numpy()
-            pred_phi = np.arctan2(output[:, 0, :], output[:, 1, :]) * 180.0 / np.pi
-            pred_psi = np.arctan2(output[:, 2, :], output[:, 3, :]) * 180.0 / np.pi
+            pred_phi = np.arctan2(output[:, 0, :], output[:, 1, :])
+            pred_psi = np.arctan2(output[:, 2, :], output[:, 3, :])
             phi_mae += compute_mae(lengths, pred_phi, act_phi)
             psi_mae += compute_mae(lengths, pred_psi, act_psi)
-            if not is_plt_initialized:
-                fig, ax = init_plot()
-                pred_line, act_line, = ax.plot(
-                    pred_phi[0], pred_psi[0], "ro", act_phi[0], act_psi[0], "bo"
-                )
-                is_plt_initialized = True
-            else:
-                pred_line.set_xdata(pred_phi[0])
-                pred_line.set_ydata(pred_psi[0])
-                act_line.set_xdata(act_phi[0])
-                act_line.set_ydata(act_psi[0])
-                fig.canvas.draw()
-                fig.canvas.flush_events()
-            # time.sleep(5)
+            visualize.plot_ramachandran(
+                pred_phi[0], pred_psi[0], act_phi[0], act_psi[0]
+            )
         loss /= test_size
         phi_mae /= test_size
         psi_mae /= test_size
-    return loss, phi_mae, psi_mae
+    logging.info("Testing loss: %f", loss)
+    logging.info("Phi MAE: %f", phi_mae)
+    logging.info("Psi MAE: %f", psi_mae)
 
 
-criterion = torch.nn.MSELoss()
-input_files = listdir("output/")
-input_files_filtered = filter_input_files(input_files)
-for model_dir in input_files_filtered:
-    print("Testing model " + model_dir)
-    model_path = "output/" + model_dir + "/best.model"
-    loss, phi_mae, psi_mae = test_model(model_path, criterion)
-    print("Testing loss: " + str(loss))
-    print("Phi MAE: " + str(phi_mae))
-    print("Psi MAE: " + str(psi_mae))
+if __name__ == "__main__":
+    criterion = torch.nn.MSELoss()
+    input_files = listdir("output/")
+    input_files_filtered = filter_input_files(input_files)
+    for model_dir in input_files_filtered:
+        print("Testing model " + model_dir)
+        model_path = "output/" + model_dir + "/best.model"
+        model = torch.load(model_path, map_location={"cuda:0": "cpu"})
+        test_model(model, criterion, 5)

training.py
@@ -1,6 +1,5 @@
 import logging
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
@@ -9,7 +8,6 @@ from constants import (
     DEVICE,
     EVAL_INTERVAL,
     HIDE_UI,
-    LEARNING_RATE,
     MINIBATCH_SIZE,
     PRINT_LOSS_INTERVAL,
     TRAINING_EPOCHS,
@@ -18,20 +16,7 @@ from constants import (
 )
 from dataloader import contruct_dataloader_from_disk
 from util import calc_rmsd, write_model_to_disk
-
-
-def init_plot():
-    plt.ion()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    plt.title("Ramachandran plot")
-    ticks = np.arange(-180, 181, 30)
-    ax.set_xticks(ticks)
-    ax.set_yticks(ticks)
-    ax.grid(which="both")
-    ax.set_ylabel("Psi")
-    ax.set_xlabel("Phi", color="black")
-    return fig, ax
+from visualize import Visualizer
 
 
 def transform_tertiary(lengths, tertiary):
@@ -68,8 +53,7 @@ def validate_model(model, criterion):
                 MINIBATCH_SIZE, 1, lengths[0], device=DEVICE, dtype=torch.float32
             )
             for i in range(MINIBATCH_SIZE):
-                om = torch.from_numpy(act_omega[i] * np.pi / 180.0)
-                pred_omega[i, 0, : lengths[i]] = om
+                pred_omega[i, 0, : lengths[i]] = torch.from_numpy(act_omega[i])
             # dihedrals will be converted from [Batch, 3, length]
             # [Length, Batch, 3] as this is the input
             # required by pnerf functions
@@ -90,6 +74,7 @@ def validate_model(model, criterion):
             actual_coords = (
                 transform_tertiary(lengths, tertiary).contiguous().view(-1, 3)
             )
+            # TODO Improve RMSD calculation
             rmsd += calc_rmsd(predicted_coords, actual_coords)
             # drmsd += calc_drmsd(predicted_coords, actual_coords)
         rmsd /= val_size
@@ -98,27 +83,24 @@ def validate_model(model, criterion):
     return val_size, loss.item(), rmsd
 
 
-def train_model(model):
+def train_model(model, criterion, optimizer):
     train_loader = contruct_dataloader_from_disk(TRAINING_FOLDER)
     train_size = len(train_loader.dataset)
-    # TODO Try various options of Adam
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    criterion = torch.nn.MSELoss()
 
     best_model_val_loss = 1e20
-    is_plt_initialized = False
+    best_model_rmsd = 1e20
+    best_rmsd = 1e20
+    visualize = None
     training_set_iter = 0
 
     while training_set_iter < TRAINING_EPOCHS:
-        pred_line = None
-        act_line = None
         running_train_loss = 0
         for i, data in enumerate(train_loader):
             # data is a tuple of tuple of numpy arrays except
             # lengths, which is a tuple of ints
             # Implies primary will be a tuple with MINIBATCH_SIZE number of elements
             # And each element will be of shape (Length,)
-            # phi, psi and omega are in degrees here
+            # phi, psi and omega are in radians here
             lengths, primary, evolutionary, act_phi, act_psi, act_omega = data
             # inp should be of shape [Batch, 41, Max_length]
             inp = model.generate_input(lengths, primary, evolutionary)
@@ -143,26 +125,24 @@ def train_model(model):
 
             if (i + 1) % EVAL_INTERVAL == 0:
                 val_size, val_loss, rmsd = validate_model(model, criterion)
+                if rmsd < best_rmsd:
+                    best_rmsd = rmsd
                 if val_loss < best_model_val_loss:
                     best_model_val_loss = val_loss
+                    best_model_rmsd = rmsd
                     write_model_to_disk(model, "best")
                 if not HIDE_UI:
                     output = output.detach().cpu().numpy()[0]
-                    pred_phi = np.arctan2(output[0, :], output[1, :]) * 180.0 / np.pi
-                    pred_psi = np.arctan2(output[2, :], output[3, :]) * 180.0 / np.pi
-                    if not is_plt_initialized:
-                        fig, ax = init_plot()
-                        pred_line, act_line, = ax.plot(
-                            pred_phi, pred_psi, "ro", act_phi[0], act_psi[0], "bo"
-                        )
-                        is_plt_initialized = True
-                    else:
-                        pred_line.set_xdata(pred_phi)
-                        pred_line.set_ydata(pred_psi)
-                        act_line.set_xdata(act_phi[0])
-                        act_line.set_ydata(act_psi[0])
-                        fig.canvas.draw()
-                        fig.canvas.flush_events()
+                    pred_phi = np.arctan2(output[0, :], output[1, :])
+                    pred_psi = np.arctan2(output[2, :], output[3, :])
+                    if not visualize:
+                        visualize = Visualizer()
+                    visualize.plot_ramachandran(
+                        pred_phi, pred_psi, act_phi[0], act_psi[0]
+                    )
                 logging.info("\tValidation loss: %.10lf, RMSD: %.10lf", val_loss, rmsd)
         write_model_to_disk(model, "latest")
         training_set_iter += 1
+    logging.info("Best model validation loss: %.10lf", best_model_val_loss)
+    logging.info("Best model RMSD: %.10lf", best_model_rmsd)
+    logging.info("Best RMSD: %.10lf", best_rmsd)